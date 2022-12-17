62f8e8c74eb0578827ee69793d7a4327398e6a04
AllyBerry <alicia.harriott24@gmail.com>
2022-12-17 13:15:27 -0500

    WishList changes

--- test/pageobjects/wishList.page.js
+++ test/pageobjects/wishList.page.js
@@ -4,37 +4,15 @@ class FavouritesPage {
         return $("#top-favorite")
     }
 
-    get signUpLink () {
-        return $("//li[@class='auth0-lock-tabs-current']//span[contains(text(),'Sign Up')]")
-    }
-
-    get emailInput () {
-        return $('[type="email"]')
-    }
-
-    get passwordInput () {
-        return $('[type="password"]')
-    }
-
-    get signUpButton () {
-        return $('[id="1-submit"]')
     
-    }
+   //async signUp () {
 
+      ////await this.signInOrRegisterButton.click()
+      //await this.emailInput.setValue(email)
+      ///await this.passwordInput.setValue(password)
+      //await this.signUpButton.click()
 
-    get incorrectEmail () {
-        return $('.auth0-lock-error-invalid-hint')
-    
-    }
-
-   async signUp (email, password) {
-
-      await this.signInOrRegisterButton.click()
-      await this.emailInput.setValue(email)
-      await this.passwordInput.setValue(password)
-      await this.signUpButton.click()
-
-   }
+  // }
 
 }
 
